bb4eb7dcb319af2c36f0d50204b3b8522bc5e894
Buker, Nick <Nick.Buker@nordstrom.com>
2018-07-12 15:59:51 -0700

    update docstrings

--- src/Asteroid.py
+++ src/Asteroid.py
@@ -5,6 +5,21 @@ import random
 class Asteroid(Sprite):
 
     def __init__(self, location, velocity, direction, size, color):
+        """ Asteroid class for Asteroids game
+
+        Parameters
+        ----------
+        location : list of ints
+            x, y pixel position of center of bullet
+        velocity : int
+            velocity of the asteroid (possible values 3, 6, 12)
+        direction : int
+            0-19 direction of the asteroid
+        size : int
+            size of the asteroid (possible values 60, 30, 15)
+        color : tuple of ints
+            RGB values for asteroid color
+        """
         Sprite.__init__(self, location, velocity, direction)
         self.direction = random.randint(0, 20)
         self.size = size
--- src/Bullet.py
+++ src/Bullet.py
@@ -9,18 +9,18 @@ class Bullet(Sprite):
         Parameters
         ----------
         location : list of ints
-            x, y pixel position of center of bullet
+            x, y pixel position of the center of the bullet
         velocity : int
-            velocity of bullet (always 30)
+            velocity of the bullet (always 30)
         direction : int
-            0-19 direction of bullet
+            0-19 direction of thevbullet
         """
         Sprite.__init__(self, location, velocity, direction)
         self.lifetime = 20
 
     def update_lifetime(self):
         """ Decays lifetime of the bullet
-        
+
         Returns
         -------
         None
