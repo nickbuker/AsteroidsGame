ce066e54d87f3eb4acd31b9ca650ba385f4a5306
Buker, Nick <Nick.Buker@nordstrom.com>
2018-07-12 16:17:13 -0700

    fix split direction

--- src/Asteroid.py
+++ src/Asteroid.py
@@ -1,5 +1,4 @@
 from Sprite import Sprite
-import random
 
 
 class Asteroid(Sprite):
@@ -10,17 +9,16 @@ class Asteroid(Sprite):
         Parameters
         ----------
         location : list of ints
-            x, y pixel position of center of bullet
+            x, y pixel position of center of the asteroid
         velocity : int
             velocity of the asteroid (possible values 3, 6, 12)
         direction : int
-            0-19 direction of the asteroid
+            0-19 direction of travel of the asteroid
         size : int
             size of the asteroid (possible values 60, 30, 15)
         color : tuple of ints
             RGB values for asteroid color
         """
         Sprite.__init__(self, location, velocity, direction)
-        self.direction = random.randint(0, 20)
         self.size = size
         self.color = color
