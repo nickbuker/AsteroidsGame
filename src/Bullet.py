163c0bf1b450122c30664fdd8376eda372adc5b8
Buker, Nick <Nick.Buker@nordstrom.com>
2018-07-12 15:54:01 -0700

    add docstrings to Bullet

--- src/Bullet.py
+++ src/Bullet.py
@@ -4,8 +4,26 @@ from Sprite import Sprite
 class Bullet(Sprite):
 
     def __init__(self, location, velocity, direction):
+        """ Bullet class for the Asteroids game
+
+        Parameters
+        ----------
+        location : list of ints
+            x, y pixel position of center of bullet
+        velocity : int
+            velocity of bullet (always 30)
+        direction : int
+            0-19 direction of bullet
+        """
         Sprite.__init__(self, location, velocity, direction)
         self.lifetime = 20
 
     def update_lifetime(self):
+        """ Decays lifetime of the bullet
+        
+        Returns
+        -------
+        None
+        """
         self.lifetime -= 1
+        return
